cf2f12d100b0cca1fe8c8e30a41f85d411b32101
Raquel Rocha <rsrocha@ua.pt>
2016-11-04 01:24:51 +0000

    Quase feito - faltam testes e rever TODOs

--- VLC_Control/VLC_Control/Request.cs
+++ VLC_Control/VLC_Control/Request.cs
@@ -7,13 +7,17 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
 
+
 namespace VLC_Control
 {
     class Request
     {
         String address = "127.0.0.1:8080";
         HttpWebRequest httpreq;
+        Dictionary<string,string> playlistAtual;
         Dictionary<string, Dictionary<string, string>> playlist;
+        int quantidade = 0;
+        HashSet<string> tipos;
         //{tipo: {nome:uri}}
 
         /*
@@ -28,8 +32,51 @@ namespace VLC_Control
                 this.address = address;
             }
 
+            tipos = new HashSet<string>();
             this.playlist = new Dictionary<string, Dictionary<string, string>>();
-            //createPlayLists();
+            this.playlistAtual = new Dictionary<string, string>();
+
+            createPlayLists();
+            updatePlayListAtual();
+        }
+        private void updatePlayListAtual() {
+            foreach (Dictionary<string, string> dic in playlist.Values) {
+                foreach (KeyValuePair<string, string> musicas in dic)
+                {
+                    if (!playlistAtual.ContainsKey(musicas.Key))
+                        playlistAtual.Add(musicas.Key, musicas.Value);
+                    else
+                        playlistAtual[musicas.Key] = musicas.Value;
+                    
+                }
+
+            }
+            updateListInVLC();
+        }
+
+        private void updateListInVLC() {
+            try
+            {
+
+                httpreq = (HttpWebRequest)WebRequest.Create("http://" + this.address + "/requests/status.xml?command=pl_empty");
+                httpreq.Headers[HttpRequestHeader.Authorization] = "Basic " + credential;
+                httpreq.GetResponse();
+            }
+            catch (HttpListenerException)
+            {
+                //Console.WriteLine("Erro no pedido. Volte a tentar");
+            }
+            foreach (KeyValuePair<string,string> musicas in playlistAtual)
+            try
+            {
+                httpreq = (HttpWebRequest)WebRequest.Create("http://" + this.address + "/requests/status.xml?command=in_enqueue&input=" + musicas.Value);
+                httpreq.Headers[HttpRequestHeader.Authorization] = "Basic " + credential;
+                httpreq.GetResponse();
+            }
+            catch (HttpListenerException)
+            {
+                //Console.WriteLine("Erro no pedido. Volte a tentar");
+            }
         }
 
         string credential = Convert.ToBase64String(Encoding.Default.GetBytes(":123456"));
@@ -61,13 +108,45 @@ namespace VLC_Control
             }
         }
 
+        public void play(string name = null, string type = null)
+        {
+            Console.WriteLine(name + "->" + type);
+            Console.WriteLine(getFile(name));
+            string mrl = getFile(name);
+            Console.WriteLine("PATH    ---- " + mrl);
+            try
+            {
+                httpreq = (HttpWebRequest)WebRequest.Create("http://" + this.address + "/requests/status.xml?command=in_play&input=" + mrl);
+                httpreq.Headers[HttpRequestHeader.Authorization] = "Basic " + credential;
+                httpreq.GetResponse();
+            }
+            catch (HttpListenerException)
+            {
+                Console.WriteLine("Erro no pedido. Volte a tentar");
+            }
+        }
+
         public void pause()
         {
             try
             {
-                httpreq = (HttpWebRequest)WebRequest.Create("http://" + this.address + "/requests/status.xml?command=pl_pause");
+
+                /*httpreq = (HttpWebRequest)WebRequest.Create("http://" + this.address + "/requests/status.xml");
                 httpreq.Headers[HttpRequestHeader.Authorization] = "Basic " + credential;
-                httpreq.GetResponse();
+                WebResponse response = httpreq.GetResponse();
+                Stream str = response.GetResponseStream();
+                XmlDocument doc = new XmlDocument();
+                doc.Load(str);
+                foreach(String x in doc.ChildNodes)
+                //if(doc["status"].FirstChild.In == "paused")
+                if (!doc["state"].FirstChild.Value.Equals("paused") && !doc["state"].FirstChild.Value.Equals("stopped"))
+                {*/
+                    httpreq = (HttpWebRequest)WebRequest.Create("http://" + this.address + "/requests/status.xml?command=pl_pause");
+                    httpreq.Headers[HttpRequestHeader.Authorization] = "Basic " + credential;
+                    httpreq.GetResponse();
+                //}
+
+                    
             }
             catch (HttpListenerException)
             {
@@ -90,6 +169,9 @@ namespace VLC_Control
                     break;
                 case -1:
                     varVol = "-25";
+                    break;
+                case 2:
+                    varVol = "+75";
                     break;
                 default:
                     varVol = "400";
@@ -126,6 +208,7 @@ namespace VLC_Control
 
         public void nextFile()//a bit bugged (problem of VLC and not ours!!!)
         {
+            Console.WriteLine("VEIO AQUI");
             try
             {
                 httpreq = (HttpWebRequest)WebRequest.Create("http://" + this.address + "/requests/status.xml?command=pl_next");
@@ -153,74 +236,59 @@ namespace VLC_Control
                 Console.WriteLine("Erro no pedido. Volte a tentar");
             }
         }
+        
+        public void loopPlaylist() {
+            try
+            {
+                httpreq = (HttpWebRequest)WebRequest.Create("http://" + this.address + "/requests/status.xml?command=pl_loop");
+                httpreq.Headers[HttpRequestHeader.Authorization] = "Basic " + credential;
+                httpreq.GetResponse();
+            }
+            catch (HttpListenerException)
+            {
+                Console.WriteLine("Erro no pedido. Volte a tentar");
+            }
+        }
+        public void repeatPlaylist() {
+            try
+            {
+                httpreq = (HttpWebRequest)WebRequest.Create("http://" + this.address + "/requests/status.xml?command=pl_repeat");
+                httpreq.Headers[HttpRequestHeader.Authorization] = "Basic " + credential;
+                httpreq.GetResponse();
+            }
+            catch (HttpListenerException)
+            {
+                Console.WriteLine("Erro no pedido. Volte a tentar");
+            }
+        }
 
-        /* TODO:
-        Verificar quais são as "tarefas que faltam implementar 
-        - incluíndo as que estão em comentário pq não estão feitas!!! */
-
-
-        /*public WebRequest openFile(String name)
+        public void randomPlaylist() {
+            try
+            {
+                httpreq = (HttpWebRequest)WebRequest.Create("http://" + this.address + "/requests/status.xml?command=pl_random");
+                httpreq.Headers[HttpRequestHeader.Authorization] = "Basic " + credential;
+                httpreq.GetResponse();
+            }
+            catch (HttpListenerException)
+            {
+                Console.WriteLine("Erro no pedido. Volte a tentar");
+            }
+        }
+        
+        public string getFile(string fname)
         {
-            //search name in directory
-            //return null in case it does not exist
-            //This will play the one added (add to playlist and plays)
-            //?command=in_play&input=<uri>
-            //This will add the file to playlist and not play it automatically
-            //?command = in_enqueue & input =< uri >
-        }*/
-
-        //public WebRequest openPlaylist(String name) { }
-
-        //public WebRequest seePlaylist() { }
-
-
-        //public WebRequest forward() { }
-        //public WebRequest backward() { }
-        /*public WebRequest repeatPlaylist() {
-            //?command=pl_repeat
-        }*/
-
-        /*public WebRequest randomPlaylist() {
-            //?command=pl_random
-        }*/
-
-        //public WebRequest changeSpeed(bool op) { } //0- speed up, 1 - speed down
-
-        /*public WebRequest addSubtitles()//Select the file respective to the movie (same name)
-        {
-            //?command = addsubtitle & val =< uri >
-            //^uri = directory!!
-        }*/
-
-        /*public WebRequest addSubtitles(Uri subtitles)//Select the file choosen 
-        {
-            //?command = addsubtitle & val =< uri >
-            //^uri = subtitles
-        }*/
-
-        /* TODO:
-            getNames of files?
-            função que faça retrieve da playlist atual?
-            função que procure a musica/filme/playlist em questão?
-            getPlaylists?
-            getCurrentMusic?
-            select playlist (nsei se ja está em cima)
-
-        */
-
-
-        /*public string getFile(string fname) {
-            foreach (KeyValuePair<string, Dictionary<string, string>> dic in playlist) {
-                Dictionary<string, string> aux = dic.Value;
-                if (aux.ContainsKey(fname)) {
-                    return aux[fname];
+            foreach (KeyValuePair<string, Dictionary<string, string>> entry in this.playlist)
+            {
+                foreach (KeyValuePair<string, string> dic in entry.Value)
+                {
+                    if (dic.Key.Equals(fname, StringComparison.InvariantCultureIgnoreCase))
+                        return dic.Value;
                 }
             }
             return "";
-        }*/
+        }
 
-        
-        /*private void createPlayLists()
+        private void createPlayLists()
         {
             string dir = System.IO.Directory.GetCurrentDirectory() + "\\musicas";
 
@@ -239,36 +307,59 @@ namespace VLC_Control
                     }
 
                     TagLib.File f = TagLib.File.Create(fi.FullName);
-                    
-                    foreach (string g in f.Tag.Genres) {
-                        Console.WriteLine(g);
-                        if (this.playlist.ContainsKey(g))
+
+                    foreach (string g in f.Tag.Genres)
+                    {
+                        if (this.playlist.ContainsKey(g) || (g.Equals("Português") && this.playlist.ContainsKey("Portuguesa")))
                         {
+                            quantidade += 1;
+                            if (g.Equals("Português"))
+                                this.playlist["Portuguesa"][f.Tag.Title] = fi.FullName;
+                            else
                                 this.playlist[g][f.Tag.Title] = fi.FullName;
                         }
                         else
                         {
                             Dictionary<string, string> dic = new Dictionary<string, string>();
                             dic.Add(f.Tag.Title, fi.FullName);
-                            this.playlist.Add(g, dic);
-                        }    
+                            
+                            if (g.Equals("Português"))
+                            {
+                                this.playlist.Add("Portuguesa", dic);
+                                tipos.Add("Portuguesa");
+                            }
+                            else{
+                                this.playlist.Add(g, dic);
+                                tipos.Add(g);
+                            }
+                                
+                        }
                     }
-                    foreach (KeyValuePair<string, Dictionary<string,string>> entry in this.playlist)
+                    Console.WriteLine("Músicas a adicionar à playlist...");
+                    foreach (KeyValuePair<string, Dictionary<string, string>> entry in this.playlist)
                     {
                         Console.Write(entry.Key + ": \n");
                         foreach (KeyValuePair<string, string> dic in entry.Value)
-                            Console.Write(dic.Key + ":\n" + dic.Value + "\n");
+                            Console.WriteLine(dic.Key + ":\n" + dic.Value + "\n");
                     }
-
-
-                    Console.WriteLine("----------");
                 }
-
-            Console.WriteLine("\n");
         }
-        string playlistActual;
-        public void playPlaylist(string playlist) {
 
-        }*/
+        public void playlistByType(string tipo) {
+            playlistAtual = new Dictionary<string, string>();
+            playlistAtual = playlist[tipo];
+            updateListInVLC();
+        }
+
+        public HashSet<string> getPlaylistNames() {
+            HashSet<string> aux = new HashSet<string>();
+            foreach (KeyValuePair<string, string> music in playlistAtual)
+                aux.Add(music.Key);
+            return aux;
+        }
+
+        public HashSet<string> getTipos() {
+            return tipos;
+        }
     }
 }
--- VLC_Control/VLC_Control/SpeechRecognizer.cs
+++ VLC_Control/VLC_Control/SpeechRecognizer.cs
@@ -8,6 +8,9 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace VLC_Control
+    /* TODO
+        Acção "Quero ouvir todas as músicas" -> voltar à playlist original    
+*/
 {
     class SpeechRecognizer
     {
@@ -41,36 +44,35 @@ namespace VLC_Control
             {
                 Console.WriteLine("Não foi possível carregar a gramática geral.");
                 tts.Speak("Não foi possível carregar a gramática geral.");
+                return;
             }
-
-            //Load of the dinamic grammar! (playlist/music names)
-            /*Grammar playlist_gr = createPlGrammar();
-            Grammar files_names_gr = createFnamesGrammar();
-
-            if (playlist_gr == null)
+            //Gramática dos ficheiros (nomes)
+            Grammar names = createFilesGrammar();
+            if (names != null)
             {
-                tts.Speak("Não foi possível carregar a gramática das listas");
-                Console.WriteLine("Não foi possível carregar a gramática das listas");
+                names.Enabled = true;
+                sre.LoadGrammar(names);
+                Console.WriteLine("Gramática de nomes carregada...");
             }
             else
             {
-                playlist_gr.Enabled = true;
-                sre.LoadGrammar(playlist_gr);
-                Console.WriteLine("Gramática das listas carregada");
+                Console.WriteLine("Não foi possível carregar a gramática de nomes.");
+                tts.Speak("Não foi possível carregar a gramática de nomes.");
             }
 
-            if (files_names_gr == null)
+            //Gramática das categorias (Rock, Pop, Comédia, etc.)
+            Grammar type = createTypeGrammar();
+            if (type != null)
             {
-                tts.Speak("Não foi possível carregar a gramática dos nomes");
-                Console.WriteLine("Não foi possível carregar a gramática dos nomes");
+                type.Enabled = true;
+                sre.LoadGrammar(type);
+                Console.WriteLine("Gramática das categorias carregada...");
             }
             else
             {
-                files_names_gr.Enabled = true;
-                sre.LoadGrammar(files_names_gr);
-                Console.WriteLine("Gramática dos nomes carregada");
+                Console.WriteLine("Não foi possível carregar a gramática das categorias.");
+                tts.Speak("Não foi possível carregar a gramática das categorias.");
             }
-            */
 
             sre.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(sre_SpeechRecognized);
             sre.RecognizeAsync(RecognizeMode.Multiple);
@@ -101,49 +103,99 @@ namespace VLC_Control
             Console.WriteLine("Confiança: " + e.Result.Confidence);
             if (e.Result.Confidence > 0.65)
             {
-                foreach (KeyValuePair<string, SemanticValue> r in e.Result.Semantics)
+                if (e.Result.Semantics.ContainsKey("reproduzir") && e.Result.Confidence > 0.4)
                 {
-                    Console.WriteLine(r.Key + ":" + r.Value.Value.ToString());
-
-                    switch (r.Value.Value.ToString())
+                    KeyValuePair<string, SemanticValue>[] sem = e.Result.Semantics.ToArray();
+                    string query = e.Result.Text;
+                    string remove = sem[0].Value.Value.ToString();
+                    query = query.Replace(remove, "").Trim();
+                    Console.WriteLine("A procurar por: " + query);
+                    tts.Speak("A procurar por: " + query);
+                    bool notfound = request.getFile(query).Equals(""); //Retorna "" se não encontrar
+                    if (notfound)
+                        tts.Speak("Não encontro a música " + query);
+                    else
                     {
-                        case "reproduzir":
-                            Console.WriteLine("XXXx");
-                            request.play();
-                            break;
-                        case "parar":
-                            request.stop();
-                            break;
-                        case "próxima":
-                            request.nextFile();
-                            break;
-                        case "anterior":
-                            request.previousFile();
-                            break;
-                        case "alto":
-                            request.changeVolume(1);
-                            break;
-                        case "baixo":
-                            request.changeVolume(-1);
-                            break;
-                        case "mute":
-                            request.changeVolume(0);
-                            break;
-                        case "pausar":
-                            request.pause();
-                            break;
-                        case "fullscreen":
-                            request.fullScreen();
-                            break;
-                        case "voz feminina":
-                            tts.changeGender(Microsoft.Speech.Synthesis.VoiceGender.Female);
-                            break;
-                        case "voz masculina":
-                            tts.changeGender(Microsoft.Speech.Synthesis.VoiceGender.Male);
-                            break;
-                        default:
-                            break;
+                        Console.WriteLine("Música " + query + "encontrada.");
+                        request.play(query);
+                    }
+                }
+                else if (e.Result.Semantics.ContainsKey("tipos") && e.Result.Confidence > 0.4)
+                {
+                    KeyValuePair<string, SemanticValue>[] sem = e.Result.Semantics.ToArray();
+                    string query = e.Result.Text;
+                    string remove = sem[0].Value.Value.ToString();
+                    query = query.Replace(remove, "").Trim();
+                    Console.WriteLine("A procurar por: " + query);
+                    tts.Speak("A procurar por: " + query);
+                    bool found = request.getTipos().Contains(query);
+                    if (!found)
+                        tts.Speak("Não existem músicas de " + query);
+                    else
+                    {
+                        Console.WriteLine("Existem músicas de " + query);
+                        request.stop();
+                        request.playlistByType(query);
+                        request.nextFile();
+                    }
+                        
+                }
+                else {
+                    foreach (KeyValuePair<string, SemanticValue> r in e.Result.Semantics)
+                    {
+                        Console.WriteLine(r.Key + ":" + r.Value.Value.ToString());
 
+                        switch (r.Value.Value.ToString())
+                        {
+                            case "reproduzir":
+                                request.play();
+                                break;
+                            case "parar":
+                                request.stop();
+                                break;
+                            case "seguinte":
+                                request.nextFile();
+                                break;
+                            case "anterior":
+                                request.previousFile();
+                                break;
+                            case "alto":
+                                request.changeVolume(1);
+                                break;
+                            case "maisalto":
+                                request.changeVolume(2);
+                                break;
+                            case "baixo":
+                                request.changeVolume(-1);
+                                break;
+                            case "mute":
+                                request.changeVolume(0);
+                                break;
+                            case "pausar":
+                                request.pause();
+                                break;
+                            case "fullscreen":
+                            case "notfullscreen":
+                                request.fullScreen();
+                                break;
+                            case "voz feminina":
+                                tts.changeGender(Microsoft.Speech.Synthesis.VoiceGender.Female);
+                                tts.Speak("Já mudei de voz");
+                                break;
+                            case "voz masculina":
+                                tts.changeGender(Microsoft.Speech.Synthesis.VoiceGender.Male);
+                                tts.Speak("Já mudei de voz");
+                                break;
+                            case "Bleed it out":
+                                request.play("Bleed it out");
+                                break;
+                            case "Rock":
+                                //request.getMusicByType("Rock");
+                                break;
+                            default:
+                                break;
+
+                        }
                     }
                 }
 
@@ -154,6 +206,59 @@ namespace VLC_Control
                 tts.Speak("Importa-se de repetir?");
             else
                 tts.Speak("Não entendi, repita se faz favor.");
+        }
+
+        private Grammar createFilesGrammar() {
+            /*
+            TODO:
+            Separar os nomes dos ficheiros para 
+            Filmes [nomes]  e Musicas[nomes]
+            Depois nesta função criar frases para filmes a frases para musicas
+            Feito agora: Apenas para musicas!
+            */
+            string[] frases = { "Quero ouvir a música" };
+            Choices frase = new Choices(frases);
+            GrammarBuilder elementoFrase = new GrammarBuilder(frase, 1, 1);
+            SemanticResultKey acaoSRK = new SemanticResultKey("reproduzir", elementoFrase);
+
+            HashSet<string> names = request.getPlaylistNames();
+            if (names == null || names.Count == 0)
+                return null;
+            Choices namesChoices = new Choices(names.ToArray());
+            GrammarBuilder namesGB = new GrammarBuilder(namesChoices, 1, 1);
+
+            GrammarBuilder total = acaoSRK.ToGrammarBuilder();
+            total.Append(namesGB);
+
+            total.Culture = new System.Globalization.CultureInfo("pt-PT");
+            Grammar names_gr = new Grammar(total);
+            names_gr.Name = "Files Grammar";
+            
+            return names_gr;
+        }
+
+        private Grammar createTypeGrammar() {
+
+            string[] frases = { "Quero ouvir músicas de", "Quero ouvir " };
+            Choices frase = new Choices(frases);
+            GrammarBuilder elementoFrase = new GrammarBuilder(frase, 1, 1);
+            SemanticResultKey acaoSRK = new SemanticResultKey("tipos", elementoFrase);
+
+            HashSet<string> tipos = request.getTipos();
+            if (tipos == null || tipos.Count == 0)
+                return null;
+            Choices tiposChoices = new Choices(tipos.ToArray());
+            GrammarBuilder tiposGB = new GrammarBuilder(tiposChoices, 1, 1);
+
+            GrammarBuilder total = acaoSRK.ToGrammarBuilder();
+            total.Append(tiposGB);
+
+            total.Culture = new System.Globalization.CultureInfo("pt-PT");
+            Grammar tipos_gr = new Grammar(total);
+            tipos_gr.Name = "Files Grammar";
+
+            
+            return tipos_gr;
         }
     }
 }
