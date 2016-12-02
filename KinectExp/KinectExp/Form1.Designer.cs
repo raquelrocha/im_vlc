fdde4ac62bb7a59b879f42310a99906032bf4653
Unknown <rsrocha@ua.pt>
2016-12-02 11:08:32 +0000

    Initial commit - Project 3

---
+++ KinectExp/KinectExp/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace VLC_KinectControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.swipeText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(32, 30);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(460, 257);
+            this.textBox1.TabIndex = 0;
+            // 
+            // swipeText
+            // 
+            this.swipeText.Location = new System.Drawing.Point(85, 330);
+            this.swipeText.Name = "swipeText";
+            this.swipeText.Size = new System.Drawing.Size(272, 20);
+            this.swipeText.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 336);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "SWIPE";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 397);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.swipeText);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox swipeText;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ KinectExp/KinectExp/UpDownGestures.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.Kinect;
+using Paelife.KinectFramework.Gestures;
+
+namespace VLC_KinectControl
+{
+    /// <summary>
+    /// An implementation of a <see cref="GestureDetector"/>
+    /// that detects swipes motions.
+    /// </summary>
+    public class UpDownGestures : GestureDetector
+    {
+        /// <summary>
+        /// The minimal lenght the swipe gesture should have.
+        /// </summary>
+        public float SwipeMinimalLength {get;set;}
+
+        /// <summary>
+        /// The maximal lenght the swipe gesture should have.
+        /// </summary>
+        public float SwipeMaximalHeight {get;set;}
+
+        /// <summary>
+        /// The minimal duration the swipe gesture should have.
+        /// </summary>
+        public int SwipeMininalDuration {get;set;}
+
+        /// <summary>
+        /// The maximal duration the swipe gesture should have.
+        /// </summary>
+        public int SwipeMaximalDuration {get;set;}
+
+
+        /// <summary>
+        /// Default constructor that receives the joint that should be tracked and the
+        /// number of recorded positions that are considered when detecting gestures.
+        /// </summary>
+        public UpDownGestures(JointType trackedJoint, int windowSize = 20)
+            : base(trackedJoint, windowSize)
+        {
+            SwipeMinimalLength = 0.4f;
+            SwipeMaximalHeight = 0.2f;
+            SwipeMininalDuration = 250;
+            SwipeMaximalDuration = 1500;
+            MinimalPeriodBetweenGestures = 1000;
+        }
+
+        //! @cond
+        protected bool ScanPositions(Func<Vector3, Vector3, bool> heightFunction, Func<Vector3, Vector3, bool> directionFunction, 
+            Func<Vector3, Vector3, bool> lengthFunction, int minTime, int maxTime)
+        {
+            int start = 0;
+
+            for (int index = 1; index < Entries.Count - 1; index++)
+            {
+                if (!heightFunction(Entries[0].Position, Entries[index].Position) || !directionFunction(Entries[index].Position, Entries[index + 1].Position))
+                {
+                    start = index;
+                }
+
+                if (lengthFunction(Entries[index].Position, Entries[start].Position))
+                {
+                    double totalMilliseconds = (Entries[index].Time - Entries[start].Time).TotalMilliseconds;
+                    if (totalMilliseconds >= minTime && totalMilliseconds <= maxTime)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+        //! @endcond
+
+        /// <summary>
+        /// The implementation of the method that will actualy look for swipes.
+        /// </summary>
+        protected override void LookForGesture()
+        {
+            // from up to down
+            if (ScanPositions((p1, p2) => Math.Abs(p2.X - p1.X) < 0.20f,
+              (p1, p2) => p2.Y - p1.Y < 0.01f, 
+              (p1, p2) => Math.Abs(p2.Y - p1.Y) > 0.2f, 250, 2500))
+            {
+                RaiseGestureDetected("SwipeDown");
+                return;
+            }
+
+            // from down to up
+            if (ScanPositions((p1, p2) => Math.Abs(p2.X - p1.X) < 0.20f,
+              (p1, p2) => p2.Y - p1.Y > -0.01f,
+              (p1, p2) => Math.Abs(p2.Y - p1.Y) > 0.2f, 250, 2500))
+            {
+                RaiseGestureDetected("SwipeUp");
+                return;
+            }
+
+        }
+    }
+}
